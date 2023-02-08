979590b9e5f130bf01afaa0866a2e8917d4da967
phagn131 <>
2023-02-08 00:33:43 -0500

    updating routes

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 // const routes = require('./public/routes/index');
-const results = require('./db/db.json')
+const db = require('./db/db.json')
+const app = express();
 
 // Server setup
 const PORT = process.env.PORT || 5001;
-const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -17,25 +17,27 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public','index.html'))
+  fs.readFileSync(path.join(__dirname, 'public', 'routes', 'index.html'))
 );
 
 // Get request
-app.get('/api/notes', (req, res) =>
-  fs.readFile(path.join('.Develop/db/db.json').then(function(data) {
-    notes = [].concat.apply(JSON.parse(data))
-  }))
-);
+app.get('/routes/notes', (req, res) =>
+fs.readFileSync(path.join(__dirname, '').then(function(data) {
+  notes = [].concat(JSON.parse(data))
+  res.json(notes);
+})
+  
+));
 
 // Post request
-app.post('/api/reviews', (req, res) => {
-  res.json(`${req.method} request received`);
-  console.info(req.rawHeaders);
-  console.info(`${req.method} request received`);
+app.post('/api/notes', (req, res) => {
+  res.json(`${req.data} request received`);
+  console.info(req.data);
+  console.info(`${req.data} request received`);
 });
 
 // Delete request
-app.get('/api/notes', (req, res) =>
+app.delete('/api/notes', (req, res) =>
   fs.readFile(path.join('.Develop/db/db.json').then(function(data) {
     notes = [].concat.apply(JSON.parse(data))
   }))
